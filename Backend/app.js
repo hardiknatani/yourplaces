12d6c7f42798b3efce91e66688720781e1546a2a
hardiknatani <hardiknatani202@gmail.com>
2022-07-09 05:50:55 +0530

    Heroku deploy configs

--- Backend/app.js
+++ Backend/app.js
@@ -35,9 +35,9 @@ app.use('/api/users', usersRoutes);
 
 const dirName1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(dirName1, "frontend/build")));
+  app.use(express.static(path.join(__dirname, "frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(dirName1, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
   console.log(__dirname)
 } else {
